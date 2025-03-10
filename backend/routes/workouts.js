87ea61b34c5cc1d39065d8180bffc1d0f2ec19d5
sazzat-tuhin <44683687+SazzatTuhin@users.noreply.github.com>
2025-03-10 23:40:54 +0600

    backend setup: Router,model,controller

---
+++ backend/routes/workouts.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const {
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutController");
+const router = express.Router();
+
+// Get all workouts
+router.get("/", getWorkouts);
+
+// Get a single workoout
+router.get("/:id", getWorkout);
+
+// post a new workout
+
+router.post("/", createWorkout);
+
+// Deleet a workout
+
+router.delete("/:id", deleteWorkout);
+
+// UPDATE a workout
+
+router.patch("/:id", updateWorkout);
+module.exports = router;
