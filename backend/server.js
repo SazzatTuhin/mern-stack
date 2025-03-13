a1b24a96c525f9665aec7a6e14c58ecb937acbba
sazzat-tuhin <44683687+SazzatTuhin@users.noreply.github.com>
2025-03-13 20:38:25 +0600

    Error Handling

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
-
+// const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
+
+// cors
+// app.use(cors()); // Allow cross-origin requests
 
 // express app
 const app = express();
 
 //middleware
-app.use(express.json());
 app.use(express.json());
 
 app.use((req, res, next) => {
